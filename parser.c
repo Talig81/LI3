83775c4b3c46c663d13916affbe6923e926e4788
Manuel Moreno <rotundix81@gmail.com>
2017-04-12 23:47:20 +0100

    tenta

--- parser.c
+++ parser.c
@@ -6,23 +6,26 @@
 #include <libxml/xmlmemory.h>
 
 
-void allArticles(xmlDocPtr doc, xmlNodePtr cur,int contador){
-    cur = cur -> xmlChildrenNode;
-    xmlChar* cena;
-	while(cur != NULL){
-        if((!xmlStrcmp(cur->name,(const xmlChar*)"id"))){
-        	contador++;
-            cena = xmlNodeListGetString(doc, cur-> xmlChildrenNode,1);
-            xmlFree(cena);
-        	}
-        else cur = cur->next;
+int allArticles(xmlDocPtr doc, xmlNodePtr cur){
+	int contador = 0;
+	xmlNodePtr parente = cur;
+	xmlNodePtr child = parente->xmlChildrenNode;
+    while(parente != NULL){
+    	child = parente -> xmlChildrenNode;
+		while(child != NULL){
+        	if((!xmlStrcmp(child->name,(const xmlChar*)"id"))){
+        		contador++;
+        		}
+        	else child = xmlNextElementSibling(child);
+    	}
+    	parente = xmlNextElementSibling(parente);
+    }  
+    printf("contador %d\n",contador);
+    return contador;
     }
-    return;
-}
 
 
 int main(int argc,char** argv){
-	int contador = 0;
 	if(argc != 2){
 		 printf("falta argumentos\n");
 		 return 0;
@@ -30,18 +33,15 @@ int main(int argc,char** argv){
     xmlDocPtr doc;
     xmlNodePtr cur;
     xmlNodePtr dad;
-
+    int contador = 0;
     doc = xmlParseFile(argv[1]);
     if(doc == NULL) return 0;
     dad = xmlDocGetRootElement(doc);
-    dad = dad -> xmlChildrenNode;
-    dad = dad -> next;
-  //  dad = dad -> next;
-  //  dad = dad -> next;
-    //cur = cur -> next;
-    //cur = cur -> next;
-    dad = xmlNextElementSibling(dad);
- 	allArticles(doc,dad,contador);
+    cur = dad -> xmlChildrenNode;
+    cur = xmlNextElementSibling(cur);
+    cur = xmlNextElementSibling(cur);
+    printf("o cur é: %s\n",cur->name);
+ 	contador = allArticles(doc,cur);
     xmlFreeDoc(doc);
     xmlCleanupParser();
     return contador;
